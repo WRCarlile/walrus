edc919b2234a87732f018039fbeb042778efb188
DJ Roychoudhury and Ryan Carlile <dj@>
2016-03-22 10:01:18 -0700

    This will add initial files and JavaScript to walrus repository

---
+++ js/java.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  // $("h1").dblclick(function() {
+  //   alert("This is a header.");
+  // });
+  $(".clickable").click(function() {
+    $("#Initially-hidden").slideToggle();
+    $("#Initially-showing").slideToggle();
+  });
+$(".clickable1").click(function() {
+    $("#local-hidden").fadeToggle();
+    $("#local-showing").fadeToggle();
+  });
+$(".clickable2").click(function() {
+    $("#bar-hidden").slideToggle();
+    $("#bar-showing").slideToggle();
+  });
+  // $("img").hover(function() {
+  //   alert("This is an image.");
+  // });
+  // $("h2").click(function() {
+  //   alert("This is a sub-header.");
+  // });
+  // $("ul").click(function() {
+  //   alert("This is a list.");
+  // });
+});
