dc81ea4971f720a969870dee7551ead0809257a3
DJ Roychoudhury and Ryan Carlile <ryan@>
2016-03-22 10:45:17 -0700

    This will add background color buttons.

--- js/java.js
+++ js/java.js
@@ -14,6 +14,8 @@ $(".clickable2").click(function() {
     $("#bar-hidden").slideToggle();
     $("#bar-showing").slideToggle();
   });
+
+
   // $("img").hover(function() {
   //   alert("This is an image.");
   // });
@@ -23,4 +25,19 @@ $(".clickable2").click(function() {
   // $("ul").click(function() {
   //   alert("This is a list.");
   // });
+});
+
+$(function(){
+  $("button#red").click(function(){
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  $("button#blue").click(function(){
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
+  $("button#green").click(function(){
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
 });
